531ffd9dcc32318c41ea6d3aaecb80b8dd20a6d1
Gerimae Oliva <gerimae.o@gmail.com>
2022-07-23 23:56:04 -0400

    fixing app.js and index.js

--- elite-exotic/src/index.js
+++ elite-exotic/src/index.js
@@ -1,11 +1,11 @@
-import './App.css';
+import App from "./App";
+import ReactDOM from "react-dom/client";
+import React from "react";
 
-function App() {
-  return (
-    <div>
-      <h1>This a test.</h1>
-    </div>
-  )
-}
 
-export default App;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);

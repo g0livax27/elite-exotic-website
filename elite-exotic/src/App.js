d4bf3d0bf57a048b17d867f983e440c7cd53ca49
Gerimae Oliva <gerimae.o@gmail.com>
2022-07-26 20:40:56 -0400

    making progress on pseudo-coding the formatting of the site

--- elite-exotic/src/App.js
+++ elite-exotic/src/App.js
@@ -3,7 +3,37 @@ import './App.css';
 function App() {
   return (
     <div>
-      <h1>This a test.</h1>
+      <header>
+        <h1>Header</h1>
+      </header>
+      <nav>
+        <h1>Nav Bar [upper-right]</h1>
+        <p>checklist for rental</p>
+      </nav>
+      <section>
+        <h1>Car Categories [as cards]</h1>
+        <p>Carousel</p>
+        <ul>
+          <li>
+            Cards w/Car Category
+          </li>
+          <li>
+            L - R arrows to scroll thru Car Category
+          </li>
+          <li>
+            Clickable Image to details of specific car
+          </li>
+          <li>
+            Car details [year, make, model]
+          </li>
+          <li>
+            5-7 pics [interior/exterior]
+          </li>
+        </ul>
+      </section>
+      <footer>
+        <h1>Footer</h1>
+      </footer>
     </div>
   )
 }
